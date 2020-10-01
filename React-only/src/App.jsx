a02e202120788a5d8e7dd08aa87e958d875a056b
jelarson <jesslarson93@gmail.com>
2020-09-30 22:15:36 -0600

    working product with correct logic

--- React-only/src/App.jsx
+++ React-only/src/App.jsx
@@ -2,18 +2,48 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import { css } from '@emotion/core'
 
+const answerWrapperCss = css`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+`
+
+const answerColumnCss = css`
+  display: flex;
+  flex-direction: column;
+  margin-left: 15px;
+  margin-right: 15px;
+`
+
 function App() {
   const [inputTextValue, setInputTextValue] = useState('')
   const [inputText, setInputText] = useState('')
   const [checked, setChecked] = useState(false)
+  const [countObject, setCountObject] = useState({})
 
   useEffect(() => {
     console.log(inputTextValue)
     if (!checked) {
       setInputTextValue(inputTextValue.toUpperCase())
     }
+    const obj = {}
+    inputTextValue
+      .split(' ')
+      .join('')
+      .split('')
+      .forEach((letter) => {
+        if (obj.hasOwnProperty(letter)) {
+          obj[letter]++
+        } else {
+          obj[letter] = 1
+        }
+      })
+    setCountObject(obj)
+
     // if case sensitive checked
-    // inputTextValue.split(' ').join('').split('')
     setInputText('')
   }, [inputTextValue])
 
@@ -35,6 +65,20 @@ function App() {
       >
         Submit
       </button>
+      <div css={answerWrapperCss}>
+        <div css={answerColumnCss}>
+          <div>Letter</div>
+          {Object.keys(countObject).map((key) => {
+            return <div>{key}</div>
+          })}
+        </div>
+        <div css={answerColumnCss}>
+          <div>Count</div>
+          {Object.values(countObject).map((value) => {
+            return <div>{value}</div>
+          })}
+        </div>
+      </div>
     </div>
   )
 }
